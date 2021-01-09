e490a3bebcb17d68d271b6f14aa51afd71633892
Travis CI <wirecard@travis-ci.org>
2021-01-09 20:50:49 +0800

    add test.jmx

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-jmeter -n -t load_test.jmx -Jhostname=www.honhai.com -l result.jtl -e -o report/
+rm -rf *.jtl report/
+#jmeter -n -t load_test.jmx -Jhostname=tbb.totolon.com -l result.jtl -e -o report/
+#jmeter -n -t load_test.jmx -Jhostname=buy.line.me -l result.jtl -e -o report/
+jmeter -n -t test.jmx -l result.jtl -e -o report/
